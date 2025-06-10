43da39179ab1343a124f26d68314592f6433ce16
justinacodes <justinaominisan@gmail.com>
2025-06-10 12:11:44 +0100

    Changes

--- app/index/page.tsx
+++ app/index/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import dynamic from 'next/dynamic';
 
-const Map = dynamic(() => import('../../components/Maps'), { ssr: false });
+// const Map = dynamic(() => import('../../components/Maps'), { ssr: false });
 
 export default function Home() {
   return (
